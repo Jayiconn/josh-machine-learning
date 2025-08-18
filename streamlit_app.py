b5ddc3d17adf1d3c534c6d6bf6df3af96e72ed3c
Jayiconn <21U3186@student.mau.edu.ng>
2025-08-18 08:55:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ import pandas as pd
 st.title('🤖Machine Learning App')
 
 st.info("This is app builds a machine learning model")
-df = pd.read_csv("species_to_country_mapping.csv")
+df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
 df
