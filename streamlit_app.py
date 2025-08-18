7aef64d1b15b81f8cda8575762f8871ab846c2b5
Jayiconn <21U3186@student.mau.edu.ng>
2025-08-18 09:05:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,5 @@ import pandas as pd
 st.title('🤖Machine Learning App')
 
 st.info("This is app builds a machine learning model")
-df = pd.read_csv("https://raw.githubusercontent.com/fabzy4L/DATA_ANALYTICS/main/BIOSTATISTICS_COURSE_P…
-…d.csv('/Users/f4L/Documents/GitHub/DATA_ANALYSIS/DATA_ANALYTICS/BIOSTATISTICS_COURSE_PROJECTS/snail-eating snakes.csv")
+df = pd.read_csv("wget https://raw.githubusercontent.com/pcarbo/qBio9_stuff/main/dogs.csv")
 df
