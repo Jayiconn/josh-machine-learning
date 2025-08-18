a12175b23c6bacff162e95ec274665271aa19d5b
Jayiconn <21U3186@student.mau.edu.ng>
2025-08-18 08:53:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
-
+import pandas as pd
 st.title('🤖Machine Learning App')
 
 st.info("This is app builds a machine learning model")
+df = pd.read_csv("species_to_country_mapping.csv")
+df
